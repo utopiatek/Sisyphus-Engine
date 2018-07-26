f23f1e890f905b707d2ef508887c41021b19d992
utopiatek <utopiatek@outlook.com>
2018-07-26 09:32:10 +0800

    初始化WebGL

--- Source/Test/Src/Main.cpp
+++ Source/Test/Src/Main.cpp
@@ -18,11 +18,46 @@ extern "C" ISESystem* _System()
 	return g_pSystem;
 }
 
+#include "emscripten/html5.h"
+
+SEInt InitWebGL()
+{
+	EmscriptenWebGLContextAttributes mAttrs;
+	emscripten_webgl_init_context_attributes(&mAttrs);
+	mAttrs.majorVersion = 2;
+	mAttrs.minorVersion = 0;
+	mAttrs.alpha = 0;
+	mAttrs.depth = 1;
+	mAttrs.stencil = 0;
+	mAttrs.antialias = 1;
+	//mAttrs.premultipliedAlpha = 0;
+	//mAttrs.preserveDrawingBuffer = 0;
+	//mAttrs.preferLowPowerToHighPerformance = 0;
+	//mAttrs.failIfMajorPerformanceCaveat = 0;
+	//mAttrs.enableExtensionsByDefault = 0;
+	//mAttrs.explicitSwapControl = 0;
+
+	EMSCRIPTEN_WEBGL_CONTEXT_HANDLE nContext = emscripten_webgl_create_context(0, &mAttrs);
+	printf("1====================\n");
+	if (nContext)
+	{
+		printf("2====================\n");
+		if (EMSCRIPTEN_RESULT_SUCCESS == emscripten_webgl_make_context_current(nContext))
+		{
+			printf("3====================\n");
+		}
+	}
+
+	return 0;
+}
+
 int main()
 {
 	printf("1----------------------------------------------\n");
 	ISECore::Get()->Test(0);
 	printf("N----------------------------------------------\n");
+
+	InitWebGL();
 
 #ifdef SE_WINDOWS_DESKTOP
 	getchar();
